d7c76833edc6d38be19581918c96143b458b29ab
Vishv <vishvsalvi10@gmail.com>
2024-10-25 23:29:45 +0530

    Authentication

---
+++ app/auth.ts
@@ -0,0 +1,29 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  secret: process.env.AUTH_SECRET,
+  pages: {
+    signIn: "/auth/login",
+  },
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    })
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        return { ...token, user };
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token && token.user) {
+        return { ...session, user: token.user };
+      }
+      return session;
+    },
+  },
+});
